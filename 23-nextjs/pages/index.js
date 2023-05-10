7c3c19c32e7c86f125dfa7f73830ee5a18aebff3
Kota Ito <boooocchi@gmail.com>
2023-05-10 15:36:18 -0700

    started section 23

---
+++ 23-nextjs/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HomePage() {
+  return <h1>the Homepage</h1>;
+}
+
+export default HomePage;
---
+++ 23-nextjs/pages/news/index.js
@@ -0,0 +1,5 @@
+function NewsPage() {
+  return <h1>the newspage</h1>;
+}
+
+export default NewsPage;
